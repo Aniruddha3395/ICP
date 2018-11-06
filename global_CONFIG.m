3fb397e24e51eebef8f7054326946ef2b35ccb1c
Aniruddha3395 <ani.shembekar@gmail.com>
2018-11-06 11:15:15 -0800

    backup_1106_1115

--- global_CONFIG.m
+++ global_CONFIG.m
@@ -2,25 +2,26 @@ global tool_F_T_tcp data_file_dir icp_dir;
 %%%%%%%%%%%%%%%%%
 % Tool frame transformation of TCP wrt Flange (T_tcp_wrt_F) tool
 % transformation is zero when pts wrt flange
+%%%%%%%%%%%%%%%%%
 tool_t = [1.7;0.7;45.6];                         % in mm
 tool_r = eul2rotm([0 0 0]);                    % in rad (alpha beta gamma rot)
 tool_F_T_tcp = [tool_r,tool_t;0 0 0 1];
-%%%%%%%%%%%%%%%%%
 icp_dir = 'C:/Users/ABB IRB120/Desktop/';
 data_file_dir = 'ICP_new/data_files/data2/';
-generate_model_data = true;
-generate_scan_traj_data = true;
-
+stl_file = 'ICP_new/STL_CAD/car_bonnet_1031_new.stl';
 True_w_T_p = [
     0.9999    0.0126         0    518.4;
    -0.0126    0.9999         0   -160.8;
          0         0    1.0000    314.3;
          0         0         0    1.0000
     ];
+%%%%%%%%%%%%%%%%%
+generate_model_data = true;
+generate_scan_traj_data = true;
 
 if generate_model_data
     [part_ptcloud,part_ptcloud_normals] = generate_part_ptcloud_from_part_STL(...
-        strcat(icp_dir,'ICP_new/STL_CAD/car_bonnet_1031_new.stl'));
+        strcat(icp_dir,stl_file));
 else
     part_ptcloud = dlmread(strcat(icp_dir,data_file_dir,'part_ptcloud.csv'));
     part_ptcloud_normals = dlmread(strcat(icp_dir,data_file_dir,'part_ptcloud_normals.csv'));
@@ -36,10 +37,12 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        DATA         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%% data1 %%%%%
-% tool_t = [-0.2;-0.01;48.6]; 
+% tool_t = [-0.2;-0.01;48.6];                    % in mm
 % tool_r = eul2rotm([0 0 0]);                    % in rad (alpha beta gamma rot)
+% tool_F_T_tcp = [tool_r,tool_t;0 0 0 1];
+% icp_dir = 'C:/Users/ABB IRB120/Desktop/';
 % data_file_dir = 'data_files/data1/';
-% stl_file = car_bonnet_1031_new.stl;
+% stl_file = 'ICP_new/STL_CAD/car_bonnet_1031_new.stl';
 % True_w_T_p = [
 %     1.0000   -0.0001    0.0002  477.4196;
 %     0.0001    1.0000    0.0014 -143.6004;
@@ -49,10 +52,12 @@ end
 
 
 %%%%%% data2 %%%%%
-% tool_t = [1.7;0.7;45.6];
+% tool_t = [1.7;0.7;45.6];                       % in mm
 % tool_r = eul2rotm([0 0 0]);                    % in rad (alpha beta gamma rot)
+% tool_F_T_tcp = [tool_r,tool_t;0 0 0 1];
+% icp_dir = 'C:/Users/ABB IRB120/Desktop/';
 % data_file_dir = 'data_files/data2/';
-% stl_file = car_bonnet_1031_new.stl;
+% stl_file = 'ICP_new/STL_CAD/car_bonnet_1031_new.stl';
 % True_w_T_p = [
 %     0.9999    0.0126         0    518.4;
 %    -0.0126    0.9999         0   -160.8;
